ea6aa7a3499f7a21920d9a1695c881b9273b2c4e
chalik <47066050+fachalik@users.noreply.github.com>
2022-08-01 02:02:21 +0700

    get dirname

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+/** @format */
+
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  var absolute_path = __dirname;
+  res.json({ status: 200, data: absolute_path }).status(200);
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
