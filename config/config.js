7d71af85fa83ab0088ccd413b6707903f4980552
chalik <47066050+fachalik@users.noreply.github.com>
2022-08-01 01:40:36 +0700

    remove .env

--- config/config.js
+++ config/config.js
@@ -1,40 +1,49 @@
 /** @format */
 
 require("dotenv").config;
-const {
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_NAME,
-  DB_HOSTNAME,
-  DB_HOSTNAME_DIALECT,
-} = require("../env");
+// const {
+//   DB_USERNAME,
+//   DB_PASSWORD,
+//   DB_NAME,
+//   DB_HOSTNAME,
+//   DB_HOSTNAME_DIALECT,
+// } = require("../env");
+
+const config = {
+  DB_HOSTNAME: "us-cdbr-east-06.cleardb.net",
+  DB_USERNAME: "b086612cfda8ba",
+  DB_PASSWORD: "e02c18c5",
+  DB_NAME: "heroku_4940490f60c5eca",
+  DB_HOSTNAME_DIALECT: "mysql",
+};
+
 module.exports = {
   local: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOSTNAME,
-    dialect: DB_HOSTNAME_DIALECT,
+    username: config.DB_USERNAME,
+    password: config.DB_PASSWORD,
+    database: config.DB_NAME,
+    host: config.DB_HOSTNAME,
+    dialect: config.DB_HOSTNAME_DIALECT,
   },
   development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOSTNAME,
-    dialect: DB_HOSTNAME_DIALECT,
+    username: config.DB_USERNAME,
+    password: config.DB_PASSWORD,
+    database: config.DB_NAME,
+    host: config.DB_HOSTNAME,
+    dialect: config.DB_HOSTNAME_DIALECT,
   },
   test: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOSTNAME,
-    dialect: DB_HOSTNAME_DIALECT,
+    username: config.DB_USERNAME,
+    password: config.DB_PASSWORD,
+    database: config.DB_NAME,
+    host: config.DB_HOSTNAME,
+    dialect: config.DB_HOSTNAME_DIALECT,
   },
   production: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOSTNAME,
-    dialect: DB_HOSTNAME_DIALECT,
+    username: config.DB_USERNAME,
+    password: config.DB_PASSWORD,
+    database: config.DB_NAME,
+    host: config.DB_HOSTNAME,
+    dialect: config.DB_HOSTNAME_DIALECT,
   },
 };
