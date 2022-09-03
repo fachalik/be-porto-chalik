96d3d4d6c1a5c32248a933b0237b06ba1ba8cc7b
chalik <47066050+fachalik@users.noreply.github.com>
2022-09-04 05:03:38 +0700

    remove comment

--- index.js
+++ index.js
@@ -28,17 +28,6 @@ app.use(
   })
 );
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
-//   res.header("Access-Control-Allow-Origin", "https://www.fachalik.web.id/"); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
-// static Images Folder
 app.use("/Images", express.static("./public/Images"));
 
 app.use("/", indexRouter);
