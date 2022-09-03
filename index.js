1c121dbd22cf757408ca56df6e6bf221c9d6c986
chalik <47066050+fachalik@users.noreply.github.com>
2022-09-04 03:37:01 +0700

    update cors domain

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "localhost:3000");
-  res.header("Access-Control-Allow-Origin", "fachalik.web.id"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header("Access-Control-Allow-Origin", "https://www.fachalik.web.id/"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
