c9083e3104394ed74b8a6e202b7e5d96b26f9075
fachalik <fachalik@gmail.com>
2023-04-01 01:29:23 +0700

    fix: something

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -5,9 +5,9 @@ require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "dngs3m82q",
+  api_key: "418522811688891",
+  api_secret: "XLiw9V2YralLGolODDRg1e4SW08",
 });
 
 module.exports = { cloudinary };
